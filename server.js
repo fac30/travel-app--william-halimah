889328960a632dc122af5c66e9f0d6638d4101c9
william-man <william.man@uymail.com>
2024-03-23 20:37:18 +0000

    session testing branch

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import "dotenv/config";
+import session from "express-session";
 
 const app = express();
+
 const PORT = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the server file
 const __dirname = path.dirname(__filename); // get the name of the directory
@@ -11,19 +13,76 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 app.set("views", path.join(__dirname, "/src/views"));
 app.set("view engine", "pug");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 3600000, secure: true },
+  })
+);
 app.use(express.static("src/styles"));
 app.use(express.static("src/assets"));
 app.use(express.static("src/modules"));
 
 app.get("/", (req, res) => {
-  res.render("pages/home/index", {
-    title: "",
-  });
+  res.render("pages/home/index");
 });
+
 app.get("/flights", (req, res) => {
-  res.render("pages/flights", {
-    title: "",
-  });
+  res.render("pages/flights", { data: req.session.apiResponseData });
+});
+
+app.get("/error", (req, res) => {
+  res.render("pages/error");
+});
+
+app.post("/submit", async (req, res) => {
+  try {
+    // const { origin } = req.body;
+
+    const apiResponse = await fetch(process.env.SKYSCANNER, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.SKYSCANNER_KEY,
+      },
+      body: JSON.stringify({
+        query: {
+          market: "UK",
+          locale: "en-GB",
+          currency: "GBP",
+          query_legs: [
+            {
+              origin_place_id: { iata: "LHR" },
+              destination_place_id: { iata: "SIN" },
+              date: {
+                year: 2024,
+                month: 12,
+                day: 22,
+              },
+            },
+          ],
+          adults: 1,
+          cabin_class: "CABIN_CLASS_ECONOMY",
+        },
+      }),
+    });
+    if (!apiResponse.ok) {
+      throw new Error(
+        `Error retrieving data from Skyscanner: ${apiResponse.statusText}`
+      );
+    }
+    const data = await apiResponse.json();
+    console.log(data.content.results.itineraries[0]);
+    req.session.apiResponseData = data;
+    res.redirect("/flights");
+  } catch (error) {
+    console.error(error.message);
+    res.send("/error");
+  }
 });
 
 app.get("*", function (req, res) {
