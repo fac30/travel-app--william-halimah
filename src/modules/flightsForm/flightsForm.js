e0f86fa9d7735ea0872fcc1797ede14061410f36
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-03-23 15:55:54 +0000

    add input variables

--- src/modules/flightsForm/flightsForm.js
+++ src/modules/flightsForm/flightsForm.js
@@ -1,1 +1,5 @@
-const origin = document.querySelector('#origin');
+const originInput = document.querySelector('#origin');
+const destinationInput = document.querySelector('#destination');
+const departInput = document.querySelector('#depart');
+const returnInput = document.querySelector('#return');
+const travellersInput = document.querySelector('#travellers');
