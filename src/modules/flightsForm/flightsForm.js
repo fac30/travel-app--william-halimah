841b399cb6bccb6e05afe19af72c9a6a4c4a0bb6
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-03-23 15:56:37 +0000

    renaming travellers to passengers

--- src/modules/flightsForm/flightsForm.js
+++ src/modules/flightsForm/flightsForm.js
@@ -2,4 +2,4 @@ const originInput = document.querySelector('#origin');
 const destinationInput = document.querySelector('#destination');
 const departInput = document.querySelector('#depart');
 const returnInput = document.querySelector('#return');
-const travellersInput = document.querySelector('#travellers');
+const passengersInput = document.querySelector('#passengers');
